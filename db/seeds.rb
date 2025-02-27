5a0e1777a2248dfe141f9f69431e27392cd7ba0e
Marc PLAGNES <m.plagnes@hotmail.fr>
2025-02-27 15:32:21 +0100

    Exercice Watch List done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,22 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'open-uri'
+require 'json'
+
+# Movie.destroy.all
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+# puts url
+
+movie_serialized = URI.parse(url).read
+movies = JSON.parse(movie_serialized)['results']
+# puts JSON.pretty_generate(movies)
+
+movies.each do |movie|
+  title = movie['title']
+  overview = movie['overview']
+  poster_url = "https://image.tmdb.org/t/p/original#{movie["/74hLDKjD5aGYOotO6esUVaeISa2.jpg"]}"
+  rating = movie['vote_average']
+
+  Movie.create(title: title, overview: overview, poster_url: poster_url, rating: rating)
+
+  puts title: title, overview: overview, poster_url: poster_url, rating: rating
+end
